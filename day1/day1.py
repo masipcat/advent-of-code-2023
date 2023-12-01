9bb790b43aa954ac2bb0f1a49f13c19a5ce06fe7
Jordi Masip <jordi@doofinder.com>
2023-12-01 11:43:43 +0100

    Day 1: part II

--- day1/day1.py
+++ day1/day1.py
@@ -1,14 +1,34 @@
+import string
 
+text_nums = [
+    ('one', '1'),
+    ('two', '2'),
+    ('three', '3'),
+    ('four', '4'),
+    ('five', '5'),
+    ('six', '6'),
+    ('seven', '7'),
+    ('eight', '8'),
+    ('nine', '9'),
+]
 
 def get_value(l):
     first_num = None
     last_num = None
-    for c in l:
-        if c >= '0' and c <= '9':
+    for i, c in enumerate(l):
+        if c in string.digits:
             if first_num is None:
                 first_num = c
             else:
                 last_num = c
+        else:
+            for text_num, num in text_nums:
+                if l[i:].startswith(text_num):
+                    if first_num is None:
+                        first_num = num
+                    else:
+                        last_num = num
+                    break
     if last_num is None:
         last_num = first_num
     return int(first_num + last_num)
