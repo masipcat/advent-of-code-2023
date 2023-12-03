8d475790c2ca9ebe6808542cf9223fa98bd09eb3
Jordi Masip <jordi@masip.cat>
2023-12-03 11:31:20 +0100

    Day 3: part II

--- day3/day3.py
+++ day3/day3.py
@@ -15,25 +15,29 @@ def find_numbers(lines):
     print(matrix)
 
     num_at_pos = {}
+    gears_at_pos = {}
 
     for real_y, l in enumerate(lines):
         for m in  re.finditer("(\d+)", l):
             num = m.group(1)
             start, end = m.span()
             num_at_pos[(start, real_y)] = num
-            print(num)
             for num_len in range(end - start):
-                matrix[real_y][start + num_len] = "X"
+                matrix[real_y][start + num_len] = num
 
         for m in re.finditer(r"[^\d]", l.strip("\n")):
             if m.group(0) == ".":
                 continue
             start, end = m.span()
-            matrix[real_y][start] = "S"
+            if m.group(0) == "*":
+                gears_at_pos[(start, real_y)] = "*"
+                matrix[real_y][start] = "*"
+            else:
+                matrix[real_y][start] = "S"
 
     print(num_at_pos)
     pp(matrix)
-    return matrix, num_at_pos
+    return matrix, num_at_pos, gears_at_pos
 
 def check_neightbours(matrix, position, num):
     len_num = len(num)
@@ -44,14 +48,36 @@ def check_neightbours(matrix, position, num):
     for offset in range(len(num)):
         for y in range(real_y-1, real_y+2):
             for x in range(real_x -1 + offset, real_x + 2 + offset):
+                # fFix y >= 0, x >= 0 ??
                 if y > 0 and y < bounds_y and x > 0 and x < bounds_x:
-                    if matrix[y][x] == "S":
+                    if matrix[y][x] in ("*", "S"):
                         return True
     return False
 
+def check_gear_neightbours(matrix, position):
+    len_num = len(num)
+    bounds_y = len(matrix)
+    bounds_x = len(matrix[0])
+    real_x, real_y = position
+
+    adjacent_numbers = set()
+
+    for y in range(real_y-1, real_y+2):
+        for x in range(real_x -1 , real_x + 2 ):
+            if y >= 0 and y < bounds_y and x >= 0 and x < bounds_x:
+                if re.match("\d+", matrix[y][x]):
+                    adjacent_numbers.add(int(matrix[y][x]))
+
+
+    if len(adjacent_numbers) == 2:
+        adjacent_numbers = list(adjacent_numbers)
+        return adjacent_numbers[0] * adjacent_numbers[1]
+    return 0
+
+
 
 with open('input3.txt', 'r') as f:
-    matrix, nums_at_pos = find_numbers(f.readlines())
+    matrix, nums_at_pos, gears_at_pos = find_numbers(f.readlines())
     part_numbers =[]
     for pos, num in nums_at_pos.items():
         part = check_neightbours(matrix, pos, num)
@@ -60,3 +86,10 @@ with open('input3.txt', 'r') as f:
     print(part_numbers)
     print(sum(part_numbers))
 
+    gears =[]
+    for pos, _ in gears_at_pos.items():
+        gear = check_gear_neightbours(matrix, pos)
+        if gear:
+            gears += [gear]
+    print(gears)
+    print(sum(gears))
