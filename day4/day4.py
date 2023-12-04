d9a2b6530da26b4b9fa5cbcde12c28f1ddee806f
Jordi Masip <jordi@doofinder.com>
2023-12-04 11:08:18 +0100

    Day 4: part II

--- day4/day4.py
+++ day4/day4.py
@@ -1,21 +1,33 @@
+from collections import defaultdict
+
+
 def get_points(card):
     card_name, numbers = card.split(":")
     winnin_num, mine_num = numbers.split("|")
     winnin_num = [int(n) for n in winnin_num.strip().split(" ") if n.strip()]
-    mine_num = [int(n) for n in mine_num.strip().split(" ")  if n.strip()]
+    mine_num = [int(n) for n in mine_num.strip().split(" ") if n.strip()]
     matches = 0
     for n in winnin_num:
         if n in mine_num:
             matches += 1
-    if matches:
-        return 2**(matches - 1)
-    else:
-        return 0
+    return matches
 
 
+with open("input4.txt", "r") as f:
 
-with open('input4.txt', 'r') as f:
-    total = 0
-    for l in f.readlines():
-        total += get_points(l)
-    print("Total", total)
+    scratchcards = defaultdict(int)
+
+    copies = defaultdict(int)
+    for i, l in enumerate(f.readlines()):
+        i += 1
+
+        scratchcards[i] += 1
+        scratchcards[i] += copies[i]
+
+        matches = get_points(l)
+
+        if matches:
+            for j in range(1, matches + 1):
+                copies[i + j] += scratchcards[i]
+
+    print("Total", sum(scratchcards.values()))
