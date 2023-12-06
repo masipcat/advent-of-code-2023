be010fc4ddbc3dd9fd30b45873b2959c676be950
Jordi Masip <jordi@masip.cat>
2023-12-06 12:02:18 +0100

    Day 6

---
+++ day6/day6.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+def num_of_ways_to_win(time, distance):
+    ways = []
+    for hold_time in range(time):
+        speed = hold_time * 1
+        race_duration = time - hold_time
+        if race_duration * speed > distance:
+            ways += [hold_time]
+    print(ways, "=", len(ways))
+    return len(ways)
+
+if __name__ == "__main__":
+    with open("input6.txt", "r") as f:
+        lines = f.readlines()
+        times = [int(t.strip()) for t in lines[0].split("Time: ")[1].split(" ") if t.strip()]
+        distances = [int(d.strip()) for d in lines[1].split("Distance: ")[1].split(" ") if d.strip()]
+
+        total = 1
+        for time, distance in zip(times, distances):
+            n_ways = num_of_ways_to_win(time, distance)
+            print(time, distance, "=", n_ways)
+            total *= n_ways
+
+        print("TOTAL", total)
+
