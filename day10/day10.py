56b9ea8db3857c17ffb98af8337bcde6085649cc
Jordi Masip <jordi@masip.cat>
2023-12-10 19:08:57 +0100

    Day 10: part II

--- day10/day10.py
+++ day10/day10.py
@@ -3,10 +3,8 @@ from unittest import mock
 
 import gc
 import sys
-
 import resource
 
-# sys.setrecursionlimit(15000)
 resource.setrlimit(resource.RLIMIT_STACK, [0x10000000, resource.RLIM_INFINITY])
 sys.setrecursionlimit(0x100000)
 
@@ -46,6 +44,7 @@ can_go = {
     "S": ["|", "L", "J", "S"],
 }
 
+
 def find_s(matrix):
     for j, row in enumerate(matrix):
         for i, col in enumerate(row):
@@ -75,19 +74,99 @@ def find_loop(start, matrix, visited):
         # hack!!!
         if next_pipe == "S" and len(visited) > 1:
             # loop ended
-            return [current_pipe]
+            return [(current_pipe, current_y, current_x)]
         if current_pipe == "S" or next_pipe in can_go[direction]:
             # print(current_pipe, f"=>", next_pipe, f"[{next_y},{next_x}]")
             loop = find_loop((next_y, next_x), matrix, visited + [(next_y, next_x)])
             if loop != []:
                 if (len(loop) + 1) > len(max_):
-                    loop.insert(0, current_pipe)
+                    loop.insert(0, (current_pipe, current_y, current_x))
                     max_ = loop
                 continue
-            print("---------")
 
-    print("MAX LOOP", len(max_))
     return max_
+
+
+def find_tiles(loop, matrix):
+    size_y, size_x = len(matrix), len(matrix[0])
+    new_matrix = [size_x * ["."] for _ in range(size_y)]
+    for c, y, x in loop:
+        new_matrix[y][x] = c
+
+    total = 0
+    for y in range(size_y):
+        for x in range(size_x):
+            is_inside = is_point_inside_loop((y, x), loop)
+            if is_inside:
+                new_matrix[y][x] = "I"
+                total += 1
+
+    return total
+
+
+pipe_direction = {
+    "F": "E",
+    "7": "W",
+    "J": "W",
+    "L": "E",
+}
+
+
+def is_point_inside_loop(point, loop):
+    """
+    https://stackoverflow.com/a/24721030
+
+    Count the number of intersections of this line with the line segments that make up the perimeter of your polygon.
+    If it is odd, the point is inside. If even, it is outside.
+    """
+
+    points_in_loop = [
+        (v[1], v[2]) for v in loop
+    ]
+
+    y, x = point
+
+    # This point is part of the loop
+    if (y, x) in points_in_loop:
+        return False
+
+    h_points_in_loop = {
+        (v[1], v[2]): v[0] for v in loop
+        if v[0] in "-|FJL7S"
+    }
+
+    pipes = []
+    for y in range(y):
+        if (y, x) in h_points_in_loop:
+            pipe = h_points_in_loop[(y, x)]
+            pipes += [pipe]
+
+    count = 0
+    prev_pipe = None
+    for pipe in pipes:
+        if pipe == "|":
+            pass
+
+        elif pipe == "S":
+            count += 1
+            prev_pipe = None
+
+        elif pipe == "-":
+            count += 1
+            prev_pipe = None
+
+        elif prev_pipe is None:
+            count += 1
+            prev_pipe = pipe
+
+        elif pipe_direction[prev_pipe] == pipe_direction[pipe]:
+            count += 1
+            prev_pipe = None
+
+        else:
+            prev_pipe = None
+
+    return count % 2 != 0
 
 
 if __name__ == "__main__":
@@ -96,4 +175,5 @@ if __name__ == "__main__":
         start = find_s(matrix)
         loop = find_loop(start, matrix, [])
         print(loop)
-        print(len(loop) / 2)
+        total = find_tiles(loop, matrix)
+        print(total)
